ea207d6daf18b53af6864fa02c6daa9c5f50177a
Christyan Duarte <duarteich@gmail.com>
2021-04-06 08:59:43 -0500

    Initial commit

---
+++ Reddiget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reddiget
+//
+//  Created by Christyan Huber Duarte Ibanez on 06/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
